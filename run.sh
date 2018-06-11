623b04bc940cc55fc8397acba1b940b88245245c
Snukne <leonardo.testolin@studenti.univr.it>
2018-06-11 07:42:20 +0200

    update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ sleep 3
 export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:~/homework3/ORB_SLAM2_mod/Examples/ROS
 gnome-terminal -e 'rosrun ORB_SLAM2 Stereo ORB_SLAM2_mod/Vocabulary/ORBvoc.txt ORB_SLAM2_mod/Examples/Stereo/EuRoC.yaml true'
 sleep 5
-gnome-terminal -e 'rosbag play --pause ~/V1_01_easy.bag /cam0/image_raw:=/camera/left/image_raw /cam1/image_raw:=/camera/right/image_raw'
+#modificare '/home/snukne/Scaricati' con il vostro path contenente la bag
+gnome-terminal -e 'rosbag play --pause /home/snukne/Scaricati/V1_01_easy.bag /cam0/image_raw:=/camera/left/image_raw /cam1/image_raw:=/camera/right/image_raw'
